59b0ddc8998d54f8b7809eab66d0211fdf4a1176
Phil Weir <phil.weir@numa.ie>
2015-08-25 13:20:19 +0100

    switch to using GS parameters

--- variables.py
+++ variables.py
@@ -1,63 +1,42 @@
 """Define simulation variables
 """
 
-from gosmart.parameters import P
-
-# Define the electrodes
-electrodes = {
-    "positive": [1, 2, 3, 4],
-    "negative": [5, 6]
-}
+from gosmart.parameters import P, R
 
 # Define the pairings of electrodes
 # For each relevant pair, define a voltage
 electrode_triples = P.CONSTANT_IRE_NEEDLEPAIR_VOLTAGE
 
-tissues = {"liver": {
-    "indices": (11,),
-    "sigma": (0.08, 0.32),
-    "threshold reversible": 35000,
-    "threshold irreversible": 70000,
-    "relative permittivity": 1.,
-}, "needles": {
-    "indices": (1, 2, 3, 4, 5, 6),
-    "relative permittivity": 1.,
-    "threshold reversible": 35000,
-    "threshold irreversible": 70000,
-    "sigma": (1e3, 1e3),
-}, "vessels": {
-    "indices": (8, 9, 10,),
-    "sigma": (0.7, 1.5),
-    "threshold reversible": 10000,
-    "threshold irreversible": 45000,
-    "relative permittivity": 1.,
-}, "tumour": {
-    "indices": (7,),
-    "sigma": (0.2, 0.7),
-    "threshold reversible": 40000,
-    "threshold irreversible": 80000,
-    "relative permittivity": 1.,
-}, "background": {
-    "indices": (12,),
-    "sigma": (0.02, 0.08),
-    "threshold reversible": 10000,
-    "threshold irreversible": 40000,
-    "relative permittivity": 1.,
-}}
+tissues_utility = {
+    "liver": ("organs", "TISSUE"),
+    "needles": ("needles", "NEEDLES"),
+    "vessels": ("vessels", "VESSELS"),
+    "tumour": ("tumours", "TUMOURS"),
+    "background": ("tissues", "BACKGROUND")
+}
+
+tissues = {}
+
+for name, (group, suffix) in tissues_utility.items():
+    tissues[name] = {
+        "indices": [r.idx for r in R.group(group)],
+        "relative permittivity": P["CONSTANT_IRE_RELATIVE_PERMITTIVITY_%s" % suffix],
+        "sigma": [P["CONSTANT_IRE_ELECTRIC_CONDUCTIVITY_%s_%s" % (limit, suffix)] for limit in ("LOWER", "UPPER")],
+        "threshold reversible": P["CONSTANT_IRE_ELECTRIC_CONDUCTIVITY_THRESHOLD_LOWER_%s" % suffix],
+        "threshold irreversible": P["CONSTANT_IRE_ELECTRIC_CONDUCTIVITY_THRESHOLD_UPPER_%s" % suffix],
+    }
 
 # Define the domain and voxel size
-dim_width = 512
-dim_height = 352
-dim_depth = 21
-dim_bits = 16
-delta_width = 0.68359 / 1000.
-delta_height = 0.68359 / 1000.
-delta_depth = 2.7500 / 1000.
-offset_x = 0.147
-offset_y = 0.12
-offset_z = 0.021
-voxel_size = delta_width * delta_height * delta_depth
+# dim_width = 512
+# dim_height = 352
+# dim_depth = 21
+# dim_bits = 16
+# delta_width = 0.68359 / 1000.
+# delta_height = 0.68359 / 1000.
+# delta_depth = 2.7500 / 1000.
+# offset_x = 0.147
+# offset_y = 0.12
+# offset_z = 0.021
+# voxel_size = delta_width * delta_height * delta_depth
 
-max_restarts = 1
-
-patient_data_location = "../../scratch/IRE/data/"
+max_restarts = 10
